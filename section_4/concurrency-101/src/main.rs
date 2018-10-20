1b0b9fab506f57b0760691d1b667a745c220ca85
Anatolij <java1cprog@yandex.ru>
2018-10-20 18:09:16 +0200

    Fix code style, remove`:?` useless `println` pattern
    Fix code style, remove`:?` useless `println` pattern

--- section_4/concurrency-101/src/main.rs
+++ section_4/concurrency-101/src/main.rs
@@ -1,16 +1,12 @@
 use std::thread;
 
 fn main() {
-    let thread_1 = thread::spawn(|| {
-    	"Hello"
-    });
+    let thread_1 = thread::spawn(|| "Hello");
 
     thread::sleep(std::time::Duration::from_millis(100));
 
-    let thread_2 = thread::spawn(|| {
-    	"Rust"
-    });
+    let thread_2 = thread::spawn(|| "Rust");
 
-    println!("{:?}", thread_2.join().unwrap());
-    println!("{:?}", thread_1.join().unwrap());
+    println!("{}", thread_2.join().unwrap());
+    println!("{}", thread_1.join().unwrap());
 }
