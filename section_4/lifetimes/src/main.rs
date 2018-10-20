96fd7d9c86aa4fd0f6367ec5df3fd731193061bf
Anatolij <java1cprog@yandex.ru>
2018-10-20 17:53:19 +0200

    addd using all methods in `fn main`

--- section_4/lifetimes/src/main.rs
+++ section_4/lifetimes/src/main.rs
@@ -1,24 +1,26 @@
-fn increase_by_five<'a>(x: &'a u16) -> u16{
-	x + 5
+fn increase_by_five<'a>(x: &'a u16) -> u16 {
+    x + 5
 }
-
 
 #[derive(Debug)]
 struct TestStruct<'a> {
-    x: &'a u32
+    x: &'a u32,
 }
 
-impl<'a> TestStruct<'a>{
-    fn return_x(&self) -> &'a u32{
-    	self.x
+impl<'a> TestStruct<'a> {
+    fn return_x(&self) -> &'a u32 {
+        self.x
     }
 }
 
-
 fn main() {
-	let ts = TestStruct{
-		x: &5
-	};
+    let mut x = 5;
+    x = increase_by_five(&x);
+    println!("{}", x);
+    
+    let ts = TestStruct { x: &5 };
 
-	println!("{:?}", ts.return_x());
+    println!("{:?}", ts.x);
+    
+    println!("{:?}", ts.return_x());
 }
